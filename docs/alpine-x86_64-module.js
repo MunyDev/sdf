188319911dcc07235d063b2b24648a4dc3c4de24
MunyDev <jeffplays1292@gmail.com>
2025-03-24 13:50:48 -0400

    Update alpine-x86_64-module.js

--- docs/alpine-x86_64-module.js
+++ docs/alpine-x86_64-module.js
@@ -15,19 +15,11 @@ Module['arguments'] = [
     '-virtfs', 'local,path=/.wasmenv,mount_tag=wasm0,security_model=passthrough,id=wasm0',
     '-netdev', 'socket,id=vmnic,connect=localhost:8888', '-device', 'virtio-net-pci,netdev=vmnic'
 ];
-let oldXHR = XMLHttpRequest;
-let tar = document.createElement('textarea');
-document.body.appendChild(tar);
-function log(e) {
-    tar.value+=e.loaded;
-}
-XMLHttpRequest = function (...args) {
-    tar.value += JSON.stringify(args);
-    let o = new oldXHR(...args);
-    o.onprogress = log;
-    return o;
-}
+
 Module['locateFile'] = function(path, prefix) {
+    let tar = document.createElement('textarea');
+document.body.appendChild(tar);
+    tar.value += "loading";
     return '/sdf/images/alpine-x86_64/' + path;
 };
 Module['mainScriptUrlOrBlob'] = '/sdf/images/alpine-x86_64/out.js'
