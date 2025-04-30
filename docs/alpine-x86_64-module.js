72bd95fb44a0216c88628780f46829bf5e7f908d
MunyDev <jeffplays1292@gmail.com>
2025-04-30 18:20:06 -0400

    Update alpine-x86_64-module.js

--- docs/alpine-x86_64-module.js
+++ docs/alpine-x86_64-module.js
@@ -3,8 +3,7 @@ if (typeof Module === 'undefined') {
 }
 onerror=alert;
 Module['arguments'] = [
-    '-nographic', '-M', 'pc', '-m', '512M', '-accel', 'tcg,tb-size=500,thread=multi',
-    '-smp', '4,sockets=4',
+    '-nographic', '-M', 'pc', '-m', '512M', '-accel', 'tcg,tb-size=1024',
     '-L', '/pack-rom/',
     '-nic', 'none',
     '-kernel', '/pack-kernel/vmlinuz-virt',
